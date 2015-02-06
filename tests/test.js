cd5044cfd2c6782d1a7d9930c27b2e3ae407761b
unknown <robertr@mlv-is-02.CHCI.com>
2015-02-06 15:21:43 -0500

    mostly work on parser, added to message elements where needed for parser, parser works for basic files

--- tests/test.js
+++ tests/test.js
@@ -1,36 +1,46 @@
-var hl7 = require('../lib/hl7');
+var fs = require('fs');
 
-var d = {
-  subcomponentSeperator: "&",
-  repititionCharacter: "~",
-  componentSeperator: "^",
-  fieldSeperator: "|"
-}
+
+var hl7 = require('../lib/hl7');
+var parser = new hl7.parser();
+//
+// var d = {
+//   subcomponentSeperator: "&",
+//   repititionCharacter: "~",
+//   componentSeperator: "^",
+//   fieldSeperator: "|"
+// }
 //
 // var c = new hl7.component(["something", "something"]);
 //
 // console.log(c.toString(d))
 //
 //
-var f = new hl7.field(["name", ["adrress1", "address2"], "city"], "name2");
-
-console.log(f.toString(d));
+// var f = new hl7.field(["name", ["adrress1", "address2"], "city"], "name2");
 //
-// var s = new hl7.segment("PID", ["name", "address"], "city");
+// console.log(f.toString(d));
+// //
+// // var s = new hl7.segment("PID", ["name", "address"], "city");
+// //
+// // console.log(s.toString(d));
+// //
+// //
+// // var h = new hl7.header("Sending Application", "Recieving Application")
+// //
+// // console.log(h.toString(d));
 //
-// console.log(s.toString(d));
+// var m = new hl7.message("Sending Application", "Recieving Application")
 //
+// m.addSegment("PID", "ID", "address", "city", "state");
 //
-// var h = new hl7.header("Sending Application", "Recieving Application")
+// console.log(m.toString());
 //
-// console.log(h.toString(d));
+// m.getSegment("PID").editField(1, f);
+//
+// console.log(m.toString());
 
-var m = new hl7.message("Sending Application", "Recieving Application")
+var s = fs.readFileSync("tests/ADT.hl7", {encoding: "utf8"})
 
-m.addSegment("PID", "ID", "address", "city", "state");
-
-console.log(m.toString());
-
-m.getSegment("PID").editField(1, f);
+var m = parser.parse(s)
 
 console.log(m.toString());
